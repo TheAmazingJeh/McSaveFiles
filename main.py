3bafe4f00cf236070db6c2ac7b340d1c098c930a
TheAmazingJeh <joehbooth2004@gmail.com>
2023-04-27 11:41:33 +0100

    Add Ability to download zips

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, random, shutil, requests, zipfile, sys
+import os, random, shutil, requests, zipfile, sys, webbrowser
 from github import Github, ContentFile, Commit
 from dotenv import load_dotenv
 from tkinter.filedialog import askopenfilename
@@ -6,16 +6,19 @@ from github.GithubException import GithubException
 from requests.exceptions import SSLError
 from time import sleep
 
-from print_colour import prRed, prGreen, prPurple, prYellow
+from print_colour import prRed, prGreen, prPurple, prYellow, print_ansi_colour as prANSI
 
 load_dotenv()
+os.system("")
 
-token = os.environ.get("GITHUB_TOKEN")
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
 GITHUB_REPO = "McSaveFiles"
 
 def split(fromfile, toDir, chunkSize=1024*1024*25): 
-    print("Splitting file into chunks of size: ", chunkSize)
+    """Splits a file into fragments of a specified size (recombined by `join`)"""
+    print(f"Splitting file into chunks of size: {int(round(chunkSize/1024))}kb ~{int(round(chunkSize/1024/1024))}mb")
+    fileExtension = fromfile.split('.')[-1]
     if not os.path.exists(toDir):                  # caller handles errors
         os.mkdir(toDir)                            # make dir, read/write parts
     else:
@@ -24,13 +27,14 @@ def split(fromfile, toDir, chunkSize=1024*1024*25):
     partNum = 0
     input = open(fromfile, 'rb')                   # use binary mode on Windows
     i = 0
+
     while 1:                                       # eof=empty string from read
         i += 1
         chunk = input.read(chunkSize)              # get next part <= chunkSize
         if not chunk: break
         partNum  = partNum+1
         filename = os.path.join(toDir, ('part%04d' % partNum))
-        fileobj  = open(f"{filename}.zip_fragment", 'wb')
+        fileobj  = open(f"{filename}.{fileExtension}_fragment", 'wb')
         fileobj.write(chunk)
         fileobj.close()                            # or simply open(  ).write(  )
     input.close(  )
@@ -39,9 +43,15 @@ def split(fromfile, toDir, chunkSize=1024*1024*25):
     return partNum
 
 def join(fromDir, toFile):
+    """Assembles a file from its fragments (created by `split`)"""
+    print(f"Joining file fragments. {len(os.listdir(fromDir))} fragments found")
+
     output = open(toFile, 'wb')
-    parts  = os.listdir(fromDir)
-    parts.sort(  )
+
+    fromDir = fromDir + "\\" + os.listdir(fromDir)[0]
+
+    parts  = os.listdir(f"{fromDir}")
+    parts.sort()
     for filename in parts:
         filepath = os.path.join(fromDir, filename)
         fileobj  = open(filepath, 'rb')
@@ -49,24 +59,38 @@ def join(fromDir, toFile):
             fileBytes = fileobj.read(1024*1024*25)
             if not fileBytes: break
             output.write(fileBytes)
-        fileobj.close(  )
-    output.close(  )
+        fileobj.close()
+    output.close()
 
 def extract(zipFile, toPath):
-    zipName = zipFile.split('/')[-1]
+    """Extracts a zip file to a specified path"""
+    zipName = zipFile.split('\\')[-1]
     with zipfile.ZipFile(zipFile, 'r') as zip_ref:
-        zip_ref.extractall(toPath+"/"+zipName.split('.')[0])
+        zip_ref.extractall(toPath+"\\"+zipName.split('.')[0])
 
-def construct_zip_from_part_zip(sourcePath, finalPath):
-    zipName = sourcePath.split('/')[-1]
+def construct_zip_from_part_zip(sourcePath, finalPath, deleteOriginal=True):
+    """Constructs a zip file from its fragments (created by `split`). Must be in a zip file"""
+    zipName = sourcePath.split('\\')[-1]
     zipPath = sourcePath.replace(zipName, '')
     temp = f"temp-{random.randint(10000, 99999)}"
     os.mkdir(f"{zipPath}/{temp}")
+    print("Extracting zip fragments")
+
     with zipfile.ZipFile(sourcePath, 'r') as zip_ref:
-        zip_ref.extractall(f"{zipPath}/{temp}")
-    join(f"{zipPath}/{temp}", finalPath)
-    try: shutil.rmtree(f"{zipPath}/{temp}")
+        zip_ref.extractall(f"{zipPath}\\{temp}")
+
+    
+    print("Joining zip fragments")
+    join(f"{zipPath}\\{temp}", finalPath)
+    print("Zip fragments joined")
+
+    print("Cleaning Up")
+    try: shutil.rmtree(f"{zipPath}\\{temp}")
     except Exception as e: print("Error deleting temp folder: ", e)
+
+    if deleteOriginal:
+        try: os.remove(sourcePath)
+        except Exception as e: print("Error deleting original zip: ", e)  
 
 def create_branch(repo, branch_name, branch_reference="master"):
     """Creates a branch from a reference branch in a Github repository"""
@@ -98,12 +122,12 @@ def upload_file(repo, file_path, branch):
             print(f"{log_text} ❌ {e}           ")
 
 def upload_directory(directory, branch=None):
-
+    """Uploads a directory to a Github repository (creates a new branch with the name of the directory)"""
     if not os.path.exists(directory):
         prRed(f"Directory {directory} does not exist")
         raise Exception(f"Directory {directory} does not exist")
 
-    g = Github(token)
+    g = Github(GITHUB_TOKEN)
     repo = g.get_user().get_repo(GITHUB_REPO)
     i = 0
     if branch == None: branch = directory.split('/')[-1]
@@ -127,6 +151,7 @@ def upload_directory(directory, branch=None):
     }
 
 def upload_zip(zip_path, branch=None):
+    """Uploads a zip file to a Github repository (creates a new branch with the name of the zip file)"""
     zipName = zip_path.split("/")[-1]
     zipPath = zip_path.replace("/"+zipName, '')
     zipName = zipName.replace(".zip", '')
@@ -147,28 +172,69 @@ def upload_zip(zip_path, branch=None):
     except Exception as e: print("Error deleting temp folder: ", e)
 
 def download_zip(GITHUB_REPO, branch, zip_path, autoDownload=True):
+    """Downloads a zip file from a Github repository
+    
+    autoDownload: If True, the file will be downloaded automatically. If False, the file will be opened in the browser, and the user will have to download it manually"""
     headers = {
-        "Authorization" : f'{token} ghp_r5***',
-        "Accept": '*.*'
+        "Authorization" : f'{GITHUB_TOKEN} ghp_r5***',
+        "Accept": '*.*',
+
     }
 
-    g = Github(token)
+    g = Github(GITHUB_TOKEN)
     OWNER = g.get_user().name
-    EXT  = 'zip'
+    EXT = "zip"
 
     url = f'https://api.github.com/repos/{OWNER}/{GITHUB_REPO}/{EXT}ball/{branch}'
-    print('url:', url)
+    
 
     if autoDownload:
-        r = requests.get(url, headers=headers)
+        print(f"Requesting download for {branch} from {GITHUB_REPO}")
+        
+        for i in range(5):
+            r = requests.get(url, headers=headers, stream=True)
+            if r.headers.get("Content-Length") is not None: break
+            print(f"Retrying... {i+1}/5", end="\r")
 
         if r.status_code == 200:
-            print('size:', len(r.content))
-            with open(f'{zip_path}\\{branch}.{EXT}', 'wb') as fh:
-                fh.write(r.content)
-            print(r.content[:10])  # display only some part
+            print(f"Request successful, downloading file {branch}.{EXT}")
+            with open(f'{zip_path}\\{branch}-download.{EXT}', 'wb') as f:
+                total_length = r.headers.get('Content-Length')
+
+                if total_length is None: # no content length header
+                    prRed("No content length header. File size will be unknown.")
+                    downloaded_kb = 0
+                    for data in r.iter_content(chunk_size=4096): # 4096 bytes
+                        downloaded_kb += int(round(len(data)/1024))
+                        f.write(data)
+                        prANSI(f"  {downloaded_kb} kb dowloaded (~{int(round(downloaded_kb/1024))} mb)", 222, end="\r")
+
+                else:
+                    dl = 0
+                    downloaded_kb = 0
+                    total_length = int(total_length)
+                    total_length_kb = int(round(total_length / 1024))
+                    for data in r.iter_content(chunk_size=4096): # 4096 bytes
+                        downloaded_kb += int(round(len(data)/1024))
+                        dl += len(data)
+                        f.write(data)
+                        done = int(50 * dl / total_length) 
+                        prANSI(f"  [{'=' * done}{' ' * (50-done)}] {downloaded_kb} kb /{total_length_kb} kb (~{int(round(total_length_kb/1024))} mb)", 222, end="\r")
+            prGreen(f"Downloaded {branch} from {GITHUB_REPO} ({downloaded_kb} kb)                                         ")
         else:
             print(r.text)   
+            prRed("Error downloading file")
+    else:
+        prPurple(f"Opening {url} in browser")
+        webbrowser.open(url)
 
+def download_and_recombine_branch(branch, final_path):
+    """Downloads a branch from a Github repository, and recombines the zip file"""
+    download_zip(GITHUB_REPO, branch, final_path)
+    construct_zip_from_part_zip(f"{final_path}\\{branch}-download.zip", f"{final_path}\\{branch}.zip") # TODO: Fix path
+    prGreen(f"Downloaded and recombined branch {branch}.")
+    print("File path:")
+    prYellow(f"{final_path}\\{branch}.zip")
+    return (f"{final_path}\\{branch}.zip")
 
-upload_zip(askopenfilename())
+download_and_recombine_branch("CreateAstral_SAVE", r"C:\Users\joehb\Documents\Coding\Personal-Python\PyGithub\downloads")
--- print_colour.py
+++ print_colour.py
@@ -6,3 +6,7 @@ def prPurple(prt):
     print("\033[95m{}\033[00m".format(prt))
 def prYellow(prt):
     print("\033[93m{}\033[00m".format(prt))
+def print_ansi_colour(text, ansi_colour, **kwargs):
+    """Prints text in the specified ANSI colour."""
+    """https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797"""
+    print(u"\u001b[38;5;" + f"{str(ansi_colour)}m{text}\033[00m", **kwargs)
